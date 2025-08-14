9add18332dd911d8f854c8491f2a241483bf3b67
amuli4 <abimulia@outlook.com>
2025-08-14 21:59:57 +0700

    init project skeleton

---
+++ src/main/java/com/abimulia/nlp/SpringNlpSqlApplication.java
@@ -0,0 +1,13 @@
+package com.abimulia.nlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNlpSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNlpSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abimulia/nlp/SpringNlpSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abimulia.nlp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNlpSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
